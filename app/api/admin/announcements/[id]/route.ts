51b536a948714a0a5de5e3b76e43c6b705d6ba3d
ivanprejoles <ivanpatrickprejoles@gmail.com>
2025-10-16 11:12:37 +0800

    testing

--- app/api/admin/announcements/[id]/route.ts
+++ app/api/admin/announcements/[id]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { userId } = await auth();
   if (!userId)
